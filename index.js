e87d250b489554c782a981209ca433f1e92c9de4
ga2006206521 <ga2006206521@gmail.com>
2022-07-05 18:52:04 +0800

    feat/index

---
+++ index.js
@@ -0,0 +1,139 @@
+// timer 倒數
+
+var time = document.getElementById("timeBox");
+var rps = document.getElementById("rpsWrapper");
+
+function timeDown() {
+  let timer = 4;
+  rps.style.display = "none";
+
+  var timeMinus = setInterval(() => {
+    timer--;
+    time.innerText = timer;
+    if (timer <= 0) {
+      clearInterval(timeMinus);
+      timer = 3;
+      time.innerText = timer;
+      time.style.display = "none";
+      rps.style.display = "block";
+    }
+  }, 1000);
+}
+
+// 第一關計時器
+
+var opponent;
+var choices = ["r", "p", "s"];
+var result = document.getElementById("result");
+var firstTimer = document.getElementById("timer");
+var part1Btn = document.getElementById("part1-btn");
+var firstTime;
+
+function firstTimerDown() {
+  var firstTime = 5;
+  var firstTimerMinus = setInterval(() => {
+    if (rps.style.display == "block" && firstTime >= 0) {
+      firstTimer.innerText = "出拳時間還有" + firstTime + "秒";
+      firstTime--;
+
+      // 點擊照片後暫停計時
+
+      part1Btn.addEventListener("click", function () {
+        clearInterval(firstTimerMinus);
+      });
+      if (firstTime <= -1) {
+        result.innerHTML = "失敗 超時了";
+        clearInterval(firstTimerMinus);
+
+        // 超時需停止點擊事件
+
+        document.getElementById("part1-btn").style.pointerEvents = "none";
+      }
+    }
+  }, 1000);
+}
+
+// 點擊後暫停計時
+
+window.onload = function () {
+  timeDown();
+  rpsPart1();
+  clear();
+};
+
+function rpsPart1() {
+  // 進畫面開始倒數
+
+  firstTimerDown();
+
+  // 點擊下方出拳
+
+  document
+    .getElementById("part1-btn")
+    .addEventListener("click", function change(e) {
+      let img = e.target;
+      let imgId = e.target.id;
+      document.getElementById("my-choice").src = imgId + ".png";
+
+      //  電腦出拳
+
+      opponent = choices[Math.floor(Math.random() * 3)];
+      document.getElementById("choice").src = opponent + ".png";
+
+      // 判斷使用者出了什麼
+
+      if (imgId === "r") {
+        img.src = "./rock.png";
+
+        // 使用者出拳
+
+        if (imgId === "r" && opponent === "r") {
+          result.innerText = "平手!";
+        } else if (imgId === "r" && opponent === "p") {
+          result.innerText = "輸了 底子不行";
+        } else {
+          result.innerText = "贏了 底子可以";
+        }
+      } else if (imgId === "p") {
+        img.src = "./cloth.png";
+
+        // 使用者出布
+
+        if (imgId === "p" && opponent === "p") {
+          result.innerText = "平手!";
+        } else if (imgId === "p" && opponent === "s") {
+          result.innerText = "輸了 底子不行";
+        } else {
+          result.innerText = "贏了 底子可以";
+        }
+      } else {
+        img.src = "./shears.png";
+
+        // 使用者出剪刀
+
+        if (imgId === "s" && opponent === "s") {
+          result.innerText = "平手!";
+        } else if (imgId === "p" && opponent === "s") {
+          result.innerText = "輸了 底子不行";
+        } else {
+          result.innerText = "贏了 底子可以";
+        }
+      }
+      // 都出完後需停止點擊事件
+      document.getElementById("part1-btn").style.pointerEvents = "none";
+    });
+}
+
+function clear() {
+  // 如果不在第一關 則清除第一關資料
+  setTimeout(() => {
+    document.getElementById("r").src = "r.png";
+    document.getElementById("p").src = "p.png";
+    document.getElementById("s").src = "s.png";
+    document.getElementById("choice").src = "plz.png";
+    document.getElementById("my-choice").src = "plz.png";
+    document.getElementById("part1-btn").style.pointerEvents = "auto";
+    document.getElementById("result").innerHTML = "";
+    firstTimerDown();
+  }, 10000);
+}
